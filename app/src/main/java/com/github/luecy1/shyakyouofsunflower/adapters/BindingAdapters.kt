8b3502cb2a276a9632caa48b8f49301dd650ec0e
luecy1 <kowaheno@gmail.com>
2019-04-05 21:45:33 +0900

    DataBinding用のAdapterの追加、修正

--- app/src/main/java/com/github/luecy1/shyakyouofsunflower/adapters/BindingAdapters.kt
+++ app/src/main/java/com/github/luecy1/shyakyouofsunflower/adapters/BindingAdapters.kt
@@ -1,20 +1,7 @@
 package com.github.luecy1.shyakyouofsunflower.adapters
 
 import android.view.View
-import android.widget.ImageView
 import androidx.databinding.BindingAdapter
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-
-@BindingAdapter("imageFromUrl")
-fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
-    if (!imageUrl.isNullOrEmpty()) {
-        Glide.with(view.context)
-            .load(imageUrl)
-            .transition(DrawableTransitionOptions.withCrossFade())
-            .into(view)
-    }
-}
 
 @BindingAdapter("isGone")
 fun bindIsGone(view: View, isGone: Boolean) {
---
+++ app/src/main/java/com/github/luecy1/shyakyouofsunflower/adapters/PlantDetailBindingAdapters.kt
@@ -0,0 +1,58 @@
+package com.github.luecy1.shyakyouofsunflower.adapters
+
+import android.text.SpannableStringBuilder
+import android.text.method.LinkMovementMethod
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.text.HtmlCompat
+import androidx.core.text.HtmlCompat.FROM_HTML_MODE_COMPACT
+import androidx.core.text.bold
+import androidx.core.text.italic
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.github.luecy1.shyakyouofsunflower.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+@BindingAdapter("imageFromUrl")
+fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
+    if (!imageUrl.isNullOrEmpty()) {
+        Glide.with(view.context)
+            .load(imageUrl)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(view)
+    }
+}
+
+@BindingAdapter("isGone")
+fun bindIsGone(view: FloatingActionButton, isGone: Boolean?) {
+    if (isGone == null || isGone) {
+        view.hide()
+    } else {
+        view.show()
+    }
+}
+
+@BindingAdapter("renderHtml")
+fun bindRenderHtml(view: TextView, desctiption: String?) {
+    if (desctiption != null) {
+        view.text = HtmlCompat.fromHtml(desctiption, FROM_HTML_MODE_COMPACT)
+        view.movementMethod = LinkMovementMethod.getInstance()
+    } else {
+        view.text = ""
+    }
+}
+
+@BindingAdapter("wateringText")
+fun bindiWateringText(textView: TextView, wateringInterval: Int) {
+    val resources = textView.context.resources
+    val quantityString = resources.getQuantityString(
+        R.plurals.watering_next_suffix,
+        wateringInterval, wateringInterval
+    )
+
+    textView.text = SpannableStringBuilder()
+        .bold { append(resources.getString(R.string.watering_needs_prefix)) }
+        .append(" ")
+        .italic { append(quantityString) }
+}
