a7a825bf0d7e68d89ba80bfbe4d37b751222738b
luecy1 <kowaheno@gmail.com>
2019-04-08 07:01:39 +0900

    PlantListViewModelの作成

--- app/src/main/java/com/github/luecy1/shyakyouofsunflower/viewmodel/PlantListViewModel.kt
+++ app/src/main/java/com/github/luecy1/shyakyouofsunflower/viewmodel/PlantListViewModel.kt
@@ -1,6 +1,35 @@
 package com.github.luecy1.shyakyouofsunflower.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.github.luecy1.shyakyouofsunflower.data.Plant
 import com.github.luecy1.shyakyouofsunflower.data.PlantRepository
 
-class PlantListViewModel(repository: PlantRepository) : ViewModel()
+class PlantListViewModel internal constructor(plantRepository: PlantRepository) : ViewModel() {
+
+    private val growZoneNumber = MutableLiveData<Int>().apply { value = NO_GROW_ZONE }
+
+    val plants: LiveData<List<Plant>> = Transformations.switchMap(growZoneNumber) {
+        if (it == NO_GROW_ZONE) {
+            plantRepository.getPlants()
+        } else {
+            plantRepository.getPlantsWithGrowZoneNumber(it)
+        }
+    }
+
+    fun setGrowZoneNumber(num: Int) {
+        growZoneNumber.value = num
+    }
+
+    fun clearGrowZoneNumber() {
+        growZoneNumber.value = NO_GROW_ZONE
+    }
+
+    fun isFiltered() = growZoneNumber.value != NO_GROW_ZONE
+
+    companion object {
+        private const val NO_GROW_ZONE = -1
+    }
+}
