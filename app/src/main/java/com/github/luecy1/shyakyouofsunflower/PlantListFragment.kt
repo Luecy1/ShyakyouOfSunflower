995a76f1b0820a272f07dd1a4ec0d42302b7e8ee
luecy1 <kowaheno@gmail.com>
2019-04-09 06:48:32 +0900

    PlantListFragmentの実装

--- app/src/main/java/com/github/luecy1/shyakyouofsunflower/PlantListFragment.kt
+++ app/src/main/java/com/github/luecy1/shyakyouofsunflower/PlantListFragment.kt
@@ -4,6 +4,7 @@ package com.github.luecy1.shyakyouofsunflower
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.github.luecy1.shyakyouofsunflower.adapters.PlantAdapter
 import com.github.luecy1.shyakyouofsunflower.databinding.FragmentPlantListBinding
@@ -36,7 +37,29 @@ class PlantListFragment : Fragment() {
         inflater.inflate(R.menu.menu_plant_list, menu)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.filter_zone -> {
+                updateData()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
     private fun subscribeUi(adapter: PlantAdapter) {
-        // TODO
+        viewModel.plants.observe(viewLifecycleOwner, Observer { plants ->
+            if (plants != null) adapter.submitList(plants)
+        })
+    }
+
+    private fun updateData() {
+        with(viewModel) {
+            if (isFiltered()) {
+                clearGrowZoneNumber()
+            } else {
+                setGrowZoneNumber(9)
+            }
+        }
     }
 }
